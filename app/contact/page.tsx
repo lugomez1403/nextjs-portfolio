4da67de180c8e4a9a5966bba6f1c0e52d0595f96
lugomez1403 <l.e.g.p.031192@gmail.com>
2024-05-09 15:21:26 -0500

    Se agrega links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/luis-enrique-gómez-pérez-3b8924203/",
+		label: "Linkedin",
+		handle: "@luis-enrique",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:l.e.g.p.031192@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "l.e.g.p.031192@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/lugomez1403",
 		label: "Github",
-		handle: "chronark",
+		handle: "lugomez1403",
 	},
 ];
 
