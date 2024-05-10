1853d54cfb45f8237e670fe7a8c571323b60252e
lugomez1403 <l.e.g.p.031192@gmail.com>
2024-05-09 21:23:39 -0500

    Se cambia a español y se repara error

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -32,13 +32,13 @@ export const Navigation: React.FC = () => {
 							href="/projects"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							Projects
+							Proyectos
 						</Link>
 						<Link
 							href="/contact"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							Contact
+							Contacto
 						</Link>
 					</div>
 
